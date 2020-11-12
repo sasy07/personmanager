4caea7d2f73aac6ccda92dbd598e173d93cf2664
MrLost <ubuntu@ubuntu2004.linuxvmimages.local>
2020-11-12 08:49:00 -0500

    07-03 end

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Person = (firstName, lastName, age) => {
+const Person = ({firstName, lastName, age}) => {
     return (
         <div>
             <p>{`${firstName} ${lastName} ${age} سال سن دارد .`}</p>
