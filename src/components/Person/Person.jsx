bc9e41f5f8d7e2066ff2c2089c375ae5a306843e
MrLost <ubuntu@ubuntu2004.linuxvmimages.local>
2020-11-19 06:38:48 -0500

    PersonJsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Person = ({ fullName, personDelete , personChange }) => {
+const Person = ({ fullName, personDelete, personChange }) => {
     return (
         <div onClick={personDelete} style={{ cursor: "pointer" }}>
             <p>{`${fullName}`}</p>
-            
+
         </div>
     );
 }
